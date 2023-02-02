6d50328b7e0c54adf814b8c460b0d847ab2f124d
JonahPeck <PeckJonahC@gmail.com>
2023-02-02 09:30:59 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(place = 'go to the office') {
+    return `This Monday, I will ${place}.`;
+}
+function wrapAdjective(flair = '*') {
+   const innerFunction = function (adjective = ' special') {
+    return`You are ${flair}${adjective}${flair}!`
+   }
+   return innerFunction;
+}
+   
+    // let wrapAdjective = adjective('*');
+    // return wrapAdjective('You are')(adjective
